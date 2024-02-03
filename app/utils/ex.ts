739f66b3ddcb1e1f89624f636741ed74f86fd0cf
SystemsFailure <allistirking422@gmail.com>
2024-02-03 23:27:55 +0300

    add unit test

---
+++ app/utils/ex.ts
@@ -0,0 +1,3 @@
+export function ex(a: number, b: number): number {
+  return a + b
+}
---
+++ tests/ex.test.ts
@@ -0,0 +1,5 @@
+import { ex } from '../app/utils/ex.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(ex(1, 2)).toBe(3)
+})
